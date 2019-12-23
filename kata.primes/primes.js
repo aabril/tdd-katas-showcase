c4f8e708df00ad6a69f9cb5aa8504af60bcb9565
Albert Abril <albert@Alberts-MacBook-Pro.local>
2019-12-23 23:48:33 +0100

    primes second test

--- kata.primes/primes.js
+++ kata.primes/primes.js
@@ -1,5 +1,7 @@
-function primes () {
-    return []
+function primes (number) {
+    const result = []
+    if(number===2) result.push(2)
+    return result
 }
 
 module.exports = primes
--- kata.primes/primes.test.js
+++ kata.primes/primes.test.js
@@ -3,3 +3,7 @@ const primes = require('./primes')
 test("1 should return []", () => {
     expect(primes(1)).toStrictEqual([])
 })
+
+test("2 should return [2]", () => {
+    expect(primes(2)).toStrictEqual([2])
+})
